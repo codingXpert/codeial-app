4a51cccd770fd1323053b0b229578874e409aa49
Vivek <vivekraj78965.vr@gmail.com>
2023-08-11 20:04:00 +0530

    Display Posts and related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,15 @@
-module.exports.home = function(req, res) {
-    return res.render('home', {
-        title:'Home'
+const Post = require("../models/post");
+
+module.exports.home = async function (req, res) {
+  try {
+    // Populate the user for each post
+    const posts = await Post.find({}).populate("user").exec();
+    return res.render("home", {
+      title: "Codeial | Home",
+      posts: posts,
     });
-}
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
+};
