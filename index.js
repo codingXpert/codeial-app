ee33a7e164483c382e0ec3b606460fa3361a1af6
Vivek <vivekraj78965.vr@gmail.com>
2023-08-18 13:53:58 +0530

    Showing the Avatar(on user's profile)

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.use(sassMiddleware({
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(express.static('./assets'));
+//make the uploads path available to the browser
+app.use('/uploads', express.static(__dirname + '/uploads'))
 app.use(expressLayouts);
 //extract style and script from subpages into the layout
 app.set('layout extractStyles', true);
