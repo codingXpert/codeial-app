876721fd187d46841192a1e9832139505e005354
Vivek <vivekraj78965.vr@gmail.com>
2023-08-18 08:51:26 +0530

    Creating a Post :: Receiving Data

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,7 +1,19 @@
-{   
-    // method to submit the form data for new post using AJAX
-    let createPost = function(){
-        let newPostForm = $('#new-post-form'); // id of the form
+    // Fetch user data when the page loads
+    $.ajax({
+        type: 'post',
+        url: '/posts/create', // Adjust the URL to match your backend API
+        success: function(userData) {
+        window.userData = userData;
+        createPost(); // Proceed to create post after fetching user data
+        },
+        error: function(error) {
+            console.log(error.responseText);
+        }
+    });
+
+    // Method to submit the form data for a new post using AJAX
+    function createPost() {
+        let newPostForm = $('#new-post-form');
 
         newPostForm.submit(function(e){
             e.preventDefault();
@@ -9,19 +21,34 @@
             $.ajax({
                 type: 'post',
                 url: '/posts/create',
-                data: newPostForm.serialize(),  // converts the data into json formate
+                data: newPostForm.serialize(),
                 success: function(data){
-                   console.log(data);
-                }, error: function(error){
+                    let newPost = newPostDom(data.data.post);
+                    $('#posts-list-container>ul').prepend(newPost);
+                },
+                error: function(error){
                     console.log(error.responseText);
                 }
-            }).done(function(res) {
-                $tbl.find("tbody").append(res);
-            }).fail(function(x, a, e) {
-                alert(e);
             });
         });
     }
-    // method to create a post in DOM
-    createPost();
-}
+
+    // Method to create a post in DOM
+    function newPostDom(post){
+        // Access user data from the stored object
+        const userName = window.userData.name;
+
+        return $(`<li id="post-${post._id}">
+                    <p>
+                        <small>
+                            <a class="delete-post-button" href="/posts/destroy/${ post.id }">X</a>
+                        </small>
+                        ${ post.content }
+                        <br>
+                        <small>
+                        ${ userName } <!-- Use the fetched user name -->
+                        </small>
+                    </p>
+                    <!-- Rest of your code ... -->
+                </li>`);
+    }
