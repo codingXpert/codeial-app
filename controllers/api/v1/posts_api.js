ec4abd4d7b0d7e376fd14ced7333bb4c69993c67
Vivek <vivekraj78965.vr@gmail.com>
2023-08-20 10:42:40 +0530

    Playing with APIs

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,44 @@
-module.exports.index = function(req , res) {
+const Post = require("../../../models/post");
+const Comment = require("../../../models/comment");
+
+module.exports.index = async function(req , res) {
+    const posts = await Post.find({})
+    .sort("-createdAt")
+    .populate("user", "-password") //ignoring the password 
+    .populate({
+      path:'comments',
+      populate: {
+        path: 'user'
+      }
+    });
+
 return res.json(200, {
     message:"List of posts",
-    post: []
+    post: posts
 })
 }
+
+module.exports.destroy = async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (!post) {
+        return res.status(404).send("Post not found");
+      }
+    //   if (post.user.toString() === req.user.id) {
+         post.deleteOne();
+  
+        // Delete comments associated with the post
+        await Comment.deleteMany({ post: req.params.id });
+        return res.json(200, {
+            message: 'Post and associated comments are deleted successfully!'
+        });
+
+    //   } else {
+    //     req.flash('error', 'You can not delete this post');
+    //     return res.status(403).send("You're not authorized to delete this post");
+    //   }
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send({error: err.message});
+    }
+  };
