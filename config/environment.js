b9b552bf23baa385747c3c8b6ec440ae572e36e5
Vivek <vivekraj78965.vr@gmail.com>
2023-09-08 18:09:34 +0530

    Environment Variables :: Production Environment

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ const development = {
 name: 'development',
 asset_path: './assets',
 session_cookie_key: 'somethingblabla',
-db: 'mongodb+srv://codeial_user:1234@cluster0.adf3vri.mongodb.net/codeial_development',
+db: process.env.MONGO_URI,
 smtp: {
     host: "smtp.sendgrid.net",
     port: 587,
@@ -21,8 +21,26 @@ smtp: {
   jwt_secret: "codeial"
 }
 
+
 const production = {
-name: 'production'
+name: 'production',
+asset_path: './assets',
+session_cookie_key: process.env.SESSION_COOKIE_KEY,
+db: process.env.MONGO_URI,
+smtp: {
+    host: "smtp.sendgrid.net",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.API_USER,
+      pass: process.env.API_PASS,
+    },
+  },
+
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: process.env.CALLBACK_URL,
+  jwt_secret: process.env.JWT_SECRET
 }
 
-module.exports = development;
+module.exports = eval(process.env.CODEIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT);
